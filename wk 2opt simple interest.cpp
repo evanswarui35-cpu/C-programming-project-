0be0d18176307e320c6654a73dbaad8f6aa9ac08
Evans CT100/G/30638/26 <evanswarui35@gmail.com>
2026-09-19 17:03:03 +0300

    Add files via upload

---
+++ wk 2opt simple interest.cpp
@@ -0,0 +1,27 @@
+// Evans Warui Mwangi
+// CT/100/30638/26
+
+#include <stdio.h>
+
+int main()
+{
+    float principal;
+    float time;
+    float rate;
+    float interest;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter time: ");
+    scanf("%f", &time);
+
+    printf("Enter rate: ");
+    scanf("%f", &rate);
+
+    interest = (principal * time * rate) / 100;
+
+    printf("Simple Interest = %.2f\n", interest);
+
+    return 0;
+}
