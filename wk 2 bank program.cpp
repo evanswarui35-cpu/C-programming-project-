fb499a68c61780e670ece9cfd79e43d05ac79d4a
Evans CT100/G/30638/26 <evanswarui35@gmail.com>
2026-09-19 10:48:18 +0300

    Add files via upload

---
+++ wk 2 bank program.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    double income;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your annual income: ");
+    scanf("%lf", &income);
+
+    if (age >= 21 && income >= 21000)
+    {
+        printf("Congratulations you qualify for a loan.\n");
+    }
+    else
+    {
+        printf("Unfortunately, we are unable to offer you a loan at this time.\n");
+    }
+
+    return 0;
+}
---
+++ wk 2 opt compound interest.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double principal;
+    double rate;
+    double time;
+    double amount;
+
+    printf("Enter principal amount: ");
+    scanf("%lf", &principal);
+
+    printf("Enter rate: ");
+    scanf("%lf", &rate);
+
+    printf("Enter time: ");
+    scanf("%lf", &time);
+
+    amount = principal * pow(1 + rate / 100, time);
+
+    printf("Compound Amount = %.2f\n", amount);
+
+    return 0;
+}
